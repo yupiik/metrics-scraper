035f263251869888f9b6b8e477fdfded986f105c
Loris Boiteux <lboiteux@yupiik.com>
2025-03-25 18:14:29 +0100

    [pages] clean branch

--- src/main/java/io/yupiik/metrics/scraper/configuration/ScrapingMode.java
+++
@@ -1,5 +0,0 @@
-package io.yupiik.metrics.scraper.configuration;
-
-public enum ScrapingMode {
-    PROMETHEUS,
-}
--- src/main/java/io/yupiik/metrics/scraper/model/elasticsearch/index/Property.java
+++
@@ -1,9 +0,0 @@
-package io.yupiik.metrics.scraper.model.elasticsearch.index;
-
-import io.yupiik.fusion.framework.build.api.json.JsonModel;
-
-@JsonModel
-public record Property(
-        String type
-) {
-}
